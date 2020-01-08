26e7ee32801b642a598ce59035826fa47c2b20e0
kubo-hide-kun <41711771+kubo-hide-kun@users.noreply.github.com>
2020-01-08 14:39:36 +0900

    Fix indetns from Tab to 4spaces

--- kadai_5.v
+++ kadai_5.v
@@ -19,28 +19,28 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module kadai_5(
-	input CLK12M,
-	output LED0,LED1,LED2
-   );
-		reg [2:0] CNT;
-		reg [23:0] DIV;
-		wire CLK1HZ;
-		always @ (posedge CLK12M) begin
-			if (DIV == 0) begin
-				DIV <= 12000000;
-				if(CNT == 7)begin
-					CNT <= 0;
-				end
-				else begin
-					CNT <= CNT + 1;
-				end;
-			end
-			else  begin
-				DIV <= DIV - 1;
-			end
-		end
-	assign CLK1HZ = DIV[23];
-	assign LED0 =~ CNT[0];
-	assign LED1 =~ CNT[1];
-	assign LED2 =~ CNT[2];
+    input CLK12M,
+    output LED0,LED1,LED2
+    );
+    reg [2:0] CNT;
+    reg [23:0] DIV;
+    wire CLK1HZ;
+    always @ (posedge CLK12M) begin
+        if (DIV == 0) begin
+            DIV <= 12000000;
+            if(CNT == 7)begin
+                CNT <= 0;
+            end
+            else begin
+                CNT <= CNT + 1;
+	    end
+        end
+        else  begin
+	    DIV <= DIV - 1;
+        end
+    end
+    assign CLK1HZ = DIV[23];
+    assign LED0 =~ CNT[0];
+    assign LED1 =~ CNT[1];
+    assign LED2 =~ CNT[2];
 endmodule
